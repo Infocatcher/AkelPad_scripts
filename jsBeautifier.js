aa7355553030970d0b4c3b57f02a7fd031ba4407
Infocatcher <infocatcher.dev.null@gmail.com>
2013-03-16 20:04:28 +0400

    Undo "Add patch for Array.prototype.lastIndexOf() and String[Number]" (reverse-merged from commit aa38b1bfd487af80e7ba33ab77603bfab195626b) Aren't needed since https://github.com/einars/js-beautify/commit/0088ff13552f269240016ac5cbfbaf88b8449c1b

--- jsBeautifier.js
+++ jsBeautifier.js
@@ -122,44 +122,6 @@ var indentChar = indentSize == 1
 //   Solution: string.replace(regexp, someString).split(someString)
 // 3) "abcde".substr(-1) doesn't work - use "abcde".slice(-1) instead
 
-// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/lastIndexOf#Compatibility
-if (!Array.prototype.lastIndexOf)
-{
-  Array.prototype.lastIndexOf = function(searchElement /*, fromIndex*/)
-  {
-    "use strict";
- 
-    if (this == null)
-      throw new TypeError();
- 
-    var t = Object(this);
-    var len = t.length >>> 0;
-    if (len === 0)
-      return -1;
- 
-    var n = len;
-    if (arguments.length > 1)
-    {
-      n = Number(arguments[1]);
-      if (n != n)
-        n = 0;
-      else if (n != 0 && n != (1 / 0) && n != -(1 / 0))
-        n = (n > 0 || -1) * Math.floor(Math.abs(n));
-    }
- 
-    var k = n >= 0
-          ? Math.min(n, len - 1)
-          : len - Math.abs(n);
- 
-    for (; k >= 0; k--)
-    {
-      if (k in t && t[k] === searchElement)
-        return k;
-    }
-    return -1;
-  };
-}
-
 //== index.html
 // When update this section, replace all document.getElementById() calls with above options
 // And leave beautify(source, syntax) and runTests() entry points
