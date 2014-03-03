1ce9cea05dffb968ed68b613f9e85a7e91a37cd0
Infocatcher <infocatcher.dev.null@gmail.com>
2014-03-03 11:35:37 +0400

    Use AKD_GETMAININFO instead of menu trick, v. 0.1.4 - 2014-03-03 Thanks to Instructor: http://akelpad.sourceforge.net/forum/viewtopic.php?p=24145#24145

--- saveStoreTime.js
+++ saveStoreTime.js
@@ -1,8 +1,9 @@
 // http://akelpad.sourceforge.net/forum/viewtopic.php?p=10403#10403
 // http://infocatcher.ucoz.net/js/akelpad_scripts/saveStoreTime.js
+// https://github.com/Infocatcher/AkelPad_scripts/blob/master/saveStoreTime.js
 
-// (c) Infocatcher 2010-2011
-// version 0.1.3 - 2011-11-16
+// (c) Infocatcher 2010-2011, 2014
+// version 0.1.4 - 2014-03-03
 
 // Save file and don't update modification time
 // Based on Instructor's code: http://akelpad.sourceforge.net/forum/viewtopic.php?p=4222#4222
@@ -11,25 +12,12 @@
 //   Call("Scripts::Main", 1, "saveStoreTime.js")
 
 var hMainWnd = AkelPad.GetMainWnd();
-var oSys = AkelPad.SystemFunction();
 
 if(hMainWnd) {
-	//var hMenuMain = oSys.Call("user32::GetMenu", hMainWnd);
-	var hMenuMain = AkelPad.SendMessage(hMainWnd, 1222 /*AKD_GETMAININFO*/, 21 /*MI_MENUMAIN*/, 0);
-	if(hMenuMain) {
-		var dwState = oSys.Call(
-			"user32::GetMenuState",
-			hMenuMain,
-			4252 /*IDM_OPTIONS_SAVETIME*/,
-			0 /*MF_BYCOMMAND*/
-		);
-		if(dwState & 0x8 /*MF_CHECKED*/) {
-			AkelPad.Command(4105 /*IDM_FILE_SAVE*/);
-		}
-		else {
-			AkelPad.Command(4252 /*IDM_OPTIONS_SAVETIME*/);
-			AkelPad.Command(4105 /*IDM_FILE_SAVE*/);
-			AkelPad.Command(4252 /*IDM_OPTIONS_SAVETIME*/);
-		}
-	}
+	var saveTime = AkelPad.SendMessage(hMainWnd, 1222 /*AKD_GETMAININFO*/, 148 /*MI_SAVETIME*/, 0);
+	if(!saveTime)
+		AkelPad.Command(4252 /*IDM_OPTIONS_SAVETIME*/);
+	AkelPad.Command(4105 /*IDM_FILE_SAVE*/);
+	if(!saveTime)
+		AkelPad.SendMessage(hMainWnd, 1219 /*AKD_SETMAININFO*/, 148 /*MI_SAVETIME*/, saveTime);
 }
