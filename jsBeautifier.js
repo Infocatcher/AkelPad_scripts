9353f784e6a78cce304fc4307044de08abe9d3c6
Infocatcher <infocatcher.dev.null@gmail.com>
2014-10-10 23:02:15 +0400

    [sync] Add css test generation https://github.com/beautify-web/js-beautify/commit/d40e024a965a56420a201a9b8d14374a66f55dfe

--- jsBeautifier.js
+++ jsBeautifier.js
@@ -5,7 +5,7 @@
 // (c) Infocatcher 2011-2014
 // version 0.2.7pre - 2014-09-19
 // Based on scripts from http://jsbeautifier.org/
-// [built from https://github.com/beautify-web/js-beautify/tree/master 2014-10-08 22:00:06 UTC]
+// [built from https://github.com/beautify-web/js-beautify/tree/master 2014-10-09 19:39:54 UTC]
 
 //===================
 // JavaScript unpacker and beautifier
@@ -3923,7 +3923,7 @@ if (isNode) {
 //== js/test/beautify-javascript-tests.js
 /*global js_beautify: true */
 
-function run_beautifier_tests(test_obj, Urlencoded, js_beautify, html_beautify, css_beautify)
+function run_javascript_tests(test_obj, Urlencoded, js_beautify, html_beautify, css_beautify)
 {
 
     var opts = {
@@ -3947,11 +3947,6 @@ function run_beautifier_tests(test_obj, Urlencoded, js_beautify, html_beautify, 
     function test_html_beautifier(input)
     {
         return html_beautify(input, opts);
-    }
-
-    function test_css_beautifier(input)
-    {
-        return css_beautify(input, opts);
     }
 
     var sanitytest;
@@ -4033,16 +4028,6 @@ function run_beautifier_tests(test_obj, Urlencoded, js_beautify, html_beautify, 
             field_expectation = expectation.replace(/content/g, 'pre{{field1}} {{field2}} {{field3}}post');
             test_fragment(field_input, field_expectation);
         }
-    }
-
-    // test css
-    function btc(input, expectation)
-    {
-        var wrapped_input, wrapped_expectation;
-
-        expectation = expectation || expectation === '' ? expectation : input;
-        sanitytest.test_function(test_css_beautifier, 'css_beautify');
-        test_fragment(input, expectation);
     }
 
     // run all tests for the given brace style ("collapse", "expand", "end-expand", or "none").
@@ -6079,66 +6064,143 @@ function run_beautifier_tests(test_obj, Urlencoded, js_beautify, html_beautify, 
             '<div>Should</div>\n\n\n' +
             '<div>preserve one newline</div>');
 
-        // css beautifier
+        return sanitytest;
+    }
+
+    return beautifier_tests();
+}
+
+if (typeof exports !== "undefined") {
+    exports.run_javascript_tests = run_javascript_tests;
+}
+//== js/test/beautify-javascript-tests.js end
+
+
+//== js/test/beautify-css-tests.js
+/*global js_beautify: true */
+
+function run_css_tests(test_obj, Urlencoded, js_beautify, html_beautify, css_beautify)
+{
+
+    var opts = {
+        indent_size: 4,
+        indent_char: ' ',
+        preserve_newlines: true,
+        jslint_happy: false,
+        keep_array_indentation: false,
+        brace_style: 'collapse',
+        space_before_conditional: true,
+        break_chained_methods: false,
+        selector_separator: '\n',
+        end_with_newline: false
+    };
+
+    function test_css_beautifier(input)
+    {
+        return css_beautify(input, opts);
+    }
+
+    var sanitytest;
+
+    // test the input on beautifier with the current flag settings
+    // does not check the indentation / surroundings as bt() does
+    function test_fragment(input, expected)
+    {
+        expected = expected || expected === '' ? expected : input;
+        sanitytest.expect(input, expected);
+        // if the expected is different from input, run it again
+        // expected output should be unchanged when run twice.
+        if (expected !== input) {
+            sanitytest.expect(expected, expected);
+        }
+    }
+
+    // test css
+    function t(input, expectation)
+    {
+        var wrapped_input, wrapped_expectation;
+
+        expectation = expectation || expectation === '' ? expectation : input;
+        sanitytest.test_function(test_css_beautifier, 'css_beautify');
+        test_fragment(input, expectation);
+    }
+
+    function unicode_char(value) {
+        return String.fromCharCode(value)
+    }
+
+    function beautifier_tests()
+    {
+        sanitytest = test_obj;
+
+        t(".tabs {}");
+
         opts.indent_size = 1;
         opts.indent_char = '\t';
         opts.selector_separator_newline = true;
-        opts.end_with_newline = true;
-        btc('', '\n');
-        btc('\n', '\n');
-        btc(".tabs{}\n", ".tabs {}\n");
-        btc(".tabs{}", ".tabs {}\n");
-
         opts.end_with_newline = false;
-        btc('', '');
-        btc('\n', '');
-        btc(".tabs{}\n", ".tabs {}");
+
+        // End With Newline - (eof = "\n")
+        opts.end_with_newline = true;
+        t('', '\n');
+        t('   .tabs{}', '   .tabs {}\n');
+        t('   \n\n.tabs{}\n\n\n\n', '   .tabs {}\n');
+        t('\n');
+
+        // End With Newline - (eof = "")
+        opts.end_with_newline = false;
+        t('');
+        t('   .tabs{}', '   .tabs {}');
+        t('   \n\n.tabs{}\n\n\n\n', '   .tabs {}');
+        t('\n', '');
+
+        //
 
         // test basic css beautifier
-        btc(".tabs {}");
-        btc(".tabs{color:red;}", ".tabs {\n\tcolor: red;\n}");
-        btc(".tabs{color:rgb(255, 255, 0)}", ".tabs {\n\tcolor: rgb(255, 255, 0)\n}");
-        btc(".tabs{background:url('back.jpg')}", ".tabs {\n\tbackground: url('back.jpg')\n}");
-        btc("#bla, #foo{color:red}", "#bla,\n#foo {\n\tcolor: red\n}");
-        btc("@media print {.tab{}}", "@media print {\n\t.tab {}\n}");
-        btc("@media print {.tab{background-image:url(foo@2x.png)}}", "@media print {\n\t.tab {\n\t\tbackground-image: url(foo@2x.png)\n\t}\n}");
+        t(".tabs {}");
+        t(".tabs{color:red;}", ".tabs {\n\tcolor: red;\n}");
+        t(".tabs{color:rgb(255, 255, 0)}", ".tabs {\n\tcolor: rgb(255, 255, 0)\n}");
+        t(".tabs{background:url('back.jpg')}", ".tabs {\n\tbackground: url('back.jpg')\n}");
+        t("#bla, #foo{color:red}", "#bla,\n#foo {\n\tcolor: red\n}");
+        t("@media print {.tab{}}", "@media print {\n\t.tab {}\n}");
+        t("@media print {.tab{background-image:url(foo@2x.png)}}", "@media print {\n\t.tab {\n\t\tbackground-image: url(foo@2x.png)\n\t}\n}");
 
-        btc("a:before {\n" +
+        t("a:before {\n" +
             "\tcontent: 'a{color:black;}\"\"\\'\\'\"\\n\\n\\na{color:black}\';\n" +
             "}");
 
         //lead-in whitespace determines base-indent.
         // lead-in newlines are stripped.
-        btc("\n\na, img {padding: 0.2px}", "a,\nimg {\n\tpadding: 0.2px\n}");
-        btc("   a, img {padding: 0.2px}", "   a,\n   img {\n   \tpadding: 0.2px\n   }");
-        btc(" \t \na, img {padding: 0.2px}", " \t a,\n \t img {\n \t \tpadding: 0.2px\n \t }");
-        btc("\n\n     a, img {padding: 0.2px}", "a,\nimg {\n\tpadding: 0.2px\n}");
+        t("\n\na, img {padding: 0.2px}", "a,\nimg {\n\tpadding: 0.2px\n}");
+        t("   a, img {padding: 0.2px}", "   a,\n   img {\n   \tpadding: 0.2px\n   }");
+        t(" \t \na, img {padding: 0.2px}", " \t a,\n \t img {\n \t \tpadding: 0.2px\n \t }");
+        t("\n\n     a, img {padding: 0.2px}", "a,\nimg {\n\tpadding: 0.2px\n}");
 
         // comments
-        btc("/* test */", "/* test */");
-        btc(".tabs{/* test */}", ".tabs {\n\t/* test */\n}");
-        btc("/* header */.tabs {}", "/* header */\n\n.tabs {}");
-        btc("/* header", "/* header");
-        btc("// comment", "// comment");
-        btc(".selector1 {\n\tmargin: 0; /* This is a comment including an url http://domain.com/path/to/file.ext */\n}",
+        t("/* test */", "/* test */");
+        t(".tabs{/* test */}", ".tabs {\n\t/* test */\n}");
+        t("/* header */.tabs {}", "/* header */\n\n.tabs {}");
+        t("/* header", "/* header");
+        t("// comment", "// comment");
+        t(".selector1 {\n\tmargin: 0; /* This is a comment including an url http://domain.com/path/to/file.ext */\n}",
             ".selector1 {\n\tmargin: 0;\n\t/* This is a comment including an url http://domain.com/path/to/file.ext */\n}")
 
         //single line comment support (less/sass)
-        btc(".tabs{\n// comment\nwidth:10px;\n}", ".tabs {\n\t// comment\n\twidth: 10px;\n}");
-        btc(".tabs{// comment\nwidth:10px;\n}", ".tabs {\n\t// comment\n\twidth: 10px;\n}");
-        btc("//comment\n.tabs{width:10px;}", "//comment\n.tabs {\n\twidth: 10px;\n}");
-        btc(".tabs{//comment\n//2nd single line comment\nwidth:10px;}", ".tabs {\n\t//comment\n\t//2nd single line comment\n\twidth: 10px;\n}");
-        btc(".tabs{width:10px;//end of line comment\n}", ".tabs {\n\twidth: 10px; //end of line comment\n}");
-        btc(".tabs{width:10px;//end of line comment\nheight:10px;}", ".tabs {\n\twidth: 10px; //end of line comment\n\theight: 10px;\n}");
-        btc(".tabs{width:10px;//end of line comment\nheight:10px;//another\n}", ".tabs {\n\twidth: 10px; //end of line comment\n\theight: 10px; //another\n}");
+        t(".tabs{\n// comment\nwidth:10px;\n}", ".tabs {\n\t// comment\n\twidth: 10px;\n}");
+        t(".tabs{// comment\nwidth:10px;\n}", ".tabs {\n\t// comment\n\twidth: 10px;\n}");
+        t("//comment\n.tabs{width:10px;}", "//comment\n.tabs {\n\twidth: 10px;\n}");
+        t(".tabs{//comment\n//2nd single line comment\nwidth:10px;}", ".tabs {\n\t//comment\n\t//2nd single line comment\n\twidth: 10px;\n}");
+        t(".tabs{width:10px;//end of line comment\n}", ".tabs {\n\twidth: 10px; //end of line comment\n}");
+        t(".tabs{width:10px;//end of line comment\nheight:10px;}", ".tabs {\n\twidth: 10px; //end of line comment\n\theight: 10px;\n}");
+        t(".tabs{width:10px;//end of line comment\nheight:10px;//another\n}", ".tabs {\n\twidth: 10px; //end of line comment\n\theight: 10px; //another\n}");
 
         // separate selectors
-        btc("#bla, #foo{color:red}", "#bla,\n#foo {\n\tcolor: red\n}");
-        btc("a, img {padding: 0.2px}", "a,\nimg {\n\tpadding: 0.2px\n}");
+        t("#bla, #foo{color:red}", "#bla,\n#foo {\n\tcolor: red\n}");
+        t("a, img {padding: 0.2px}", "a,\nimg {\n\tpadding: 0.2px\n}");
 
         // block nesting
-        btc("#foo {\n\tbackground-image: url(foo@2x.png);\n\t@font-face {\n\t\tfont-family: 'Bitstream Vera Serif Bold';\n\t\tsrc: url('http://developer.mozilla.org/@api/deki/files/2934/=VeraSeBd.ttf');\n\t}\n}");
-        btc("@media screen {\n\t#foo:hover {\n\t\tbackground-image: url(foo@2x.png);\n\t}\n\t@font-face {\n\t\tfont-family: 'Bitstream Vera Serif Bold';\n\t\tsrc: url('http://developer.mozilla.org/@api/deki/files/2934/=VeraSeBd.ttf');\n\t}\n}");
+        t("#foo {\n\tbackground-image: url(foo@2x.png);\n\t@font-face {\n\t\tfont-family: 'Bitstream Vera Serif Bold';\n\t\tsrc: url('http://developer.mozilla.org/@api/deki/files/2934/=VeraSeBd.ttf');\n\t}\n}");
+        t("@media screen {\n\t#foo:hover {\n\t\tbackground-image: url(foo@2x.png);\n\t}\n\t@font-face {\n\t\tfont-family: 'Bitstream Vera Serif Bold';\n\t\tsrc: url('http://developer.mozilla.org/@api/deki/files/2934/=VeraSeBd.ttf');\n\t}\n}");
 /*
 @font-face {
     font-family: 'Bitstream Vera Serif Bold';
@@ -6158,17 +6220,17 @@ function run_beautifier_tests(test_obj, Urlencoded, js_beautify, html_beautify, 
     }
 }
 */
-        btc("@font-face {\n\tfont-family: 'Bitstream Vera Serif Bold';\n\tsrc: url('http://developer.mozilla.org/@api/deki/files/2934/=VeraSeBd.ttf');\n}\n@media screen {\n\t#foo:hover {\n\t\tbackground-image: url(foo.png);\n\t}\n\t@media screen and (min-device-pixel-ratio: 2) {\n\t\t@font-face {\n\t\t\tfont-family: 'Helvetica Neue'\n\t\t}\n\t\t#foo:hover {\n\t\t\tbackground-image: url(foo@2x.png);\n\t\t}\n\t}\n}");
+        t("@font-face {\n\tfont-family: 'Bitstream Vera Serif Bold';\n\tsrc: url('http://developer.mozilla.org/@api/deki/files/2934/=VeraSeBd.ttf');\n}\n@media screen {\n\t#foo:hover {\n\t\tbackground-image: url(foo.png);\n\t}\n\t@media screen and (min-device-pixel-ratio: 2) {\n\t\t@font-face {\n\t\t\tfont-family: 'Helvetica Neue'\n\t\t}\n\t\t#foo:hover {\n\t\t\tbackground-image: url(foo@2x.png);\n\t\t}\n\t}\n}");
 
         // less-css cases
-        btc('.well{@well-bg:@bg-color;@well-fg:@fg-color;}','.well {\n\t@well-bg: @bg-color;\n\t@well-fg: @fg-color;\n}');
-        btc('.well {&.active {\nbox-shadow: 0 1px 1px @border-color, 1px 0 1px @border-color;}}',
+        t('.well{@well-bg:@bg-color;@well-fg:@fg-color;}','.well {\n\t@well-bg: @bg-color;\n\t@well-fg: @fg-color;\n}');
+        t('.well {&.active {\nbox-shadow: 0 1px 1px @border-color, 1px 0 1px @border-color;}}',
             '.well {\n' +
             '\t&.active {\n' +
             '\t\tbox-shadow: 0 1px 1px @border-color, 1px 0 1px @border-color;\n' +
             '\t}\n' +
             '}');
-        btc('a {\n' +
+        t('a {\n' +
             '\tcolor: blue;\n' +
             '\t&:hover {\n' +
             '\t\tcolor: green;\n' +
@@ -6180,64 +6242,64 @@ function run_beautifier_tests(test_obj, Urlencoded, js_beautify, html_beautify, 
 
         // Not sure if this is sensible
         // but I believe it is correct to not remove the space in "&: hover".
-        btc('a {\n' +
+        t('a {\n' +
             '\t&: hover {\n' +
             '\t\tcolor: green;\n' +
             '\t}\n' +
             '}');
 
         // import
-        btc('@import "test";');
+        t('@import "test";');
 
         // don't break nested pseudo-classes
-        btc("a:first-child{color:red;div:first-child{color:black;}}",
+        t("a:first-child{color:red;div:first-child{color:black;}}",
             "a:first-child {\n\tcolor: red;\n\tdiv:first-child {\n\t\tcolor: black;\n\t}\n}");
 
-        btc("a:first-child,a:first-child{color:red;div:first-child,div:hover{color:black;}}",
+        t("a:first-child,a:first-child{color:red;div:first-child,div:hover{color:black;}}",
             "a:first-child,\na:first-child {\n\tcolor: red;\n\tdiv:first-child, div:hover {\n\t\tcolor: black;\n\t}\n}");
 
         // handle SASS/LESS parent reference
-        btc("div{&:first-letter {text-transform: uppercase;}}",
+        t("div{&:first-letter {text-transform: uppercase;}}",
             "div {\n\t&:first-letter {\n\t\ttext-transform: uppercase;\n\t}\n}");
 
         //nested modifiers (&:hover etc)
-        btc(".tabs{&:hover{width:10px;}}", ".tabs {\n\t&:hover {\n\t\twidth: 10px;\n\t}\n}");
-        btc(".tabs{&.big{width:10px;}}", ".tabs {\n\t&.big {\n\t\twidth: 10px;\n\t}\n}");
-        btc(".tabs{&>big{width:10px;}}", ".tabs {\n\t&>big {\n\t\twidth: 10px;\n\t}\n}");
-        btc(".tabs{&+.big{width:10px;}}", ".tabs {\n\t&+.big {\n\t\twidth: 10px;\n\t}\n}");
+        t(".tabs{&:hover{width:10px;}}", ".tabs {\n\t&:hover {\n\t\twidth: 10px;\n\t}\n}");
+        t(".tabs{&.big{width:10px;}}", ".tabs {\n\t&.big {\n\t\twidth: 10px;\n\t}\n}");
+        t(".tabs{&>big{width:10px;}}", ".tabs {\n\t&>big {\n\t\twidth: 10px;\n\t}\n}");
+        t(".tabs{&+.big{width:10px;}}", ".tabs {\n\t&+.big {\n\t\twidth: 10px;\n\t}\n}");
 
         //nested rules
-        btc(".tabs{.child{width:10px;}}", ".tabs {\n\t.child {\n\t\twidth: 10px;\n\t}\n}");
+        t(".tabs{.child{width:10px;}}", ".tabs {\n\t.child {\n\t\twidth: 10px;\n\t}\n}");
 
         //variables
-        btc("@myvar:10px;.tabs{width:10px;}", "@myvar: 10px;\n.tabs {\n\twidth: 10px;\n}");
-        btc("@myvar:10px; .tabs{width:10px;}", "@myvar: 10px;\n.tabs {\n\twidth: 10px;\n}");
+        t("@myvar:10px;.tabs{width:10px;}", "@myvar: 10px;\n.tabs {\n\twidth: 10px;\n}");
+        t("@myvar:10px; .tabs{width:10px;}", "@myvar: 10px;\n.tabs {\n\twidth: 10px;\n}");
 
         // test options
         opts.indent_size = 2;
         opts.indent_char = ' ';
         opts.selector_separator_newline = false;
 
-        btc("#bla, #foo{color:green}", "#bla, #foo {\n  color: green\n}");
-        btc("@media print {.tab{}}", "@media print {\n  .tab {}\n}");
-        btc("@media print {.tab,.bat{}}", "@media print {\n  .tab, .bat {}\n}");
-        btc("#bla, #foo{color:black}", "#bla, #foo {\n  color: black\n}");
+        t("#bla, #foo{color:green}", "#bla, #foo {\n  color: green\n}");
+        t("@media print {.tab{}}", "@media print {\n  .tab {}\n}");
+        t("@media print {.tab,.bat{}}", "@media print {\n  .tab, .bat {}\n}");
+        t("#bla, #foo{color:black}", "#bla, #foo {\n  color: black\n}");
 
         // pseudo-classes and pseudo-elements
-        btc("#foo:hover {\n  background-image: url(foo@2x.png)\n}");
-        btc("#foo *:hover {\n  color: purple\n}");
-        btc("::selection {\n  color: #ff0000;\n}");
+        t("#foo:hover {\n  background-image: url(foo@2x.png)\n}");
+        t("#foo *:hover {\n  color: purple\n}");
+        t("::selection {\n  color: #ff0000;\n}");
 
         // TODO: don't break nested pseduo-classes
-        btc("@media screen {.tab,.bat:hover {color:red}}", "@media screen {\n  .tab, .bat:hover {\n    color: red\n  }\n}");
+        t("@media screen {.tab,.bat:hover {color:red}}", "@media screen {\n  .tab, .bat:hover {\n    color: red\n  }\n}");
 
         // particular edge case with braces and semicolons inside tags that allows custom text
-        btc("a:not(\"foobar\\\";{}omg\"){\ncontent: 'example\\';{} text';\ncontent: \"example\\\";{} text\";}",
+        t("a:not(\"foobar\\\";{}omg\"){\ncontent: 'example\\';{} text';\ncontent: \"example\\\";{} text\";}",
             "a:not(\"foobar\\\";{}omg\") {\n  content: 'example\\';{} text';\n  content: \"example\\\";{} text\";\n}");
 
         // may not eat the space before "["
-        btc('html.js [data-custom="123"] {\n  opacity: 1.00;\n}');
-        btc('html.js *[data-custom="123"] {\n  opacity: 1.00;\n}');
+        t('html.js [data-custom="123"] {\n  opacity: 1.00;\n}');
+        t('html.js *[data-custom="123"] {\n  opacity: 1.00;\n}');
 
         return sanitytest;
     }
@@ -6246,12 +6308,8 @@ function run_beautifier_tests(test_obj, Urlencoded, js_beautify, html_beautify, 
 }
 
 if (typeof exports !== "undefined") {
-    exports.run_beautifier_tests = run_beautifier_tests;
+    exports.run_css_tests = run_css_tests;
 }
-//== js/test/beautify-javascript-tests.js end
-
-
-//== js/test/beautify-css-tests.js
 //== js/test/beautify-css-tests.js end
 
 
