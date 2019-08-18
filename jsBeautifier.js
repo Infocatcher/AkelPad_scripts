be01ba41f1171cd07733d95881057b5ed2880a44
Infocatcher <infocatcher.dev.null@gmail.com>
2019-08-18 19:52:17 +0300

    Update source branch: gh-pages -> release

--- jsBeautifier.js
+++ jsBeautifier.js
@@ -59,7 +59,7 @@
 //   -keepCSSIndentation=true      - keep initial CSS indentation (only for -css=true)
 
 //   -update=1                     - update source from https://github.com/beautify-web/js-beautify/
-//          =2                     - update source from https://github.com/beautify-web/js-beautify/tree/gh-pages
+//          =2                     - update source from https://github.com/beautify-web/js-beautify/tree/release
 //   -forceNoCache                 - prevent caching during update
 //   -test=1                       - force run the tests
 //        =-1                      - run the tests, if called for empty source (+ see -onlySelected argument)
@@ -33269,13 +33269,13 @@ function convertSource(file, text) {
 }
 function selfUpdate() {
 	var baseUrl = update == 2
-		? "https://raw.githubusercontent.com/beautify-web/js-beautify/gh-pages/"
+		? "https://raw.githubusercontent.com/beautify-web/js-beautify/release/"
 		: "https://raw.githubusercontent.com/beautify-web/js-beautify/master/";
 	var sourceUrl = update == 2
-		? "https://github.com/beautify-web/js-beautify/tree/gh-pages"
+		? "https://github.com/beautify-web/js-beautify/tree/release"
 		: "https://github.com/beautify-web/js-beautify/tree/master";
 	var rssUrl = update == 2
-		? "https://github.com/beautify-web/js-beautify/commits/gh-pages.atom"
+		? "https://github.com/beautify-web/js-beautify/commits/release.atom"
 		: "https://github.com/beautify-web/js-beautify/commits/master.atom";
 	var data = {
 		"js/lib/beautify.js": "",
