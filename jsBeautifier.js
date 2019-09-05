1eeeb29b09ea8a35575cc43930d7435fc2ff683e
Infocatcher <infocatcher.dev.null@gmail.com>
2019-09-05 21:26:35 +0300

    [sync] Update unpacker_filter() https://github.com/beautify-web/js-beautify/blob/master/web/common-function.js

--- jsBeautifier.js
+++ jsBeautifier.js
@@ -333,38 +333,40 @@ function looks_like_html(source) {
     return trimmed && (trimmed.substring(0, 1) === '<');
 }
 function unpacker_filter(source) {
-    var trailing_comments = '',
-        comment = '',
-        unpacked = '',
-        found = false;
+  var leading_comments = '',
+    comment = '',
+    unpacked = '',
+    found = false;
 
-    // cut trailing comments
-    do {
-        found = false;
-        if (/^\s*\/\*/.test(source)) {
-            found = true;
-            comment = source.substr(0, source.indexOf('*/') + 2);
-            source = source.substr(comment.length).replace(/^\s+/, '');
-            trailing_comments += comment + "\n";
-        } else if (/^\s*\/\//.test(source)) {
-            found = true;
-            comment = source.match(/^\s*\/\/.*/)[0];
-            source = source.substr(comment.length).replace(/^\s+/, '');
-            trailing_comments += comment + "\n";
-        }
-    } while (found);
-
-    var unpackers = [P_A_C_K_E_R, Urlencoded, JavascriptObfuscator/*, MyObfuscate*/];
-    for (var i = 0; i < unpackers.length; i++) {
-        if (unpackers[i].detect(source)) {
-            unpacked = unpackers[i].unpack(source);
-            if (unpacked != source) {
-                source = unpacker_filter(unpacked);
-            }
-        }
+  // cuts leading comments
+  do {
+    found = false;
+    if (/^\s*\/\*/.test(source)) {
+      found = true;
+      comment = source.substr(0, source.indexOf('*/') + 2);
+      source = source.substr(comment.length);
+      leading_comments += comment;
+    } else if (/^\s*\/\//.test(source)) {
+      found = true;
+      comment = source.match(/^\s*\/\/.*/)[0];
+      source = source.substr(comment.length);
+      leading_comments += comment;
     }
+  } while (found);
+  leading_comments += '\n';
+  source = source.replace(/^\s+/, '');
 
-    return trailing_comments + source;
+  var unpackers = [P_A_C_K_E_R, Urlencoded, JavascriptObfuscator /*, MyObfuscate*/ ];
+  for (var i = 0; i < unpackers.length; i++) {
+    if (unpackers[i].detect(source)) {
+      unpacked = unpackers[i].unpack(source);
+      if (unpacked !== source) {
+        source = unpacker_filter(unpacked);
+      }
+    }
+  }
+
+  return leading_comments + source;
 }
 function detectXMLType(str) {
 	if(str.indexOf("<?xml") == -1)
