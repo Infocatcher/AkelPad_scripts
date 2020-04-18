6d7ef1a4de9ff341f8d7d0f2e37ca0e3ebcc0dd0
Infocatcher <infocatcher.dev.null@gmail.com>
2020-04-18 22:53:55 +0300

    Fix ratio from fxexchangerate.com: we convert all currencies into BASE_CURRENCY (USD), 1 foo => N USD and fxexchangerate.com now uses values for 1 USD, 1 USD => N foo

--- measuresConverter.js
+++ measuresConverter.js
@@ -2008,7 +2008,7 @@ function getRatioFromResponse(response, code) {
 		response.substr(0, 12) == "var fxrates="
 		&& new RegExp('\\["' + code + '"\\]=(\\d+(\\.\\d+)?);').test(response)
 	)
-		return validateRatio(stringToNumber(RegExp.$1));
+		return 1/validateRatio(stringToNumber(RegExp.$1));
 
 	return NaN;
 }
