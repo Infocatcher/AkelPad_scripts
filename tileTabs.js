0fbea95b51097bea9bb63136ab7878b40046bd33
Infocatcher <infocatcher.dev.null@gmail.com>
2021-10-16 00:09:30 +0300

    Check for horizontal/verticall scroll to get size of available area

--- tileTabs.js
+++ tileTabs.js
@@ -180,6 +180,12 @@ function tileTabs(lpFrame, lpFrame2, tileHorizontal, useTabsOrder) {
 	AkelPad.SendMessage(hMdiClient, 0x0223 /*WM_MDIRESTORE*/, hWndMdi2, 0);
 	AkelPad.SendMessage(hMdiClient, 0x0223 /*WM_MDIRESTORE*/, hWndMdi, 0);
 
+	var nStyle = oSys.Call("User32::GetWindowLong" + _TCHAR, hMdiClient, -16 /*GWL_STYLE*/);
+	if(nStyle & 0x000100000 /*WS_HSCROLL*/)
+		h -= oSys.Call("user32::GetSystemMetrics", 3 /*SM_CYHSCROLL*/) || 16;
+	if(nStyle & 0x000200000 /*WS_VSCROLL*/)
+		w -= oSys.Call("user32::GetSystemMetrics", 2 /*SM_CXVSCROLL*/) || 16;
+
 	if(tileHorizontal) {
 		var h1 = Math.floor(h/2);
 		var h2 = h - h1;
