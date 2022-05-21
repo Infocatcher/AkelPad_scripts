2f375c8675e8708e84a803a45bea4b89197e04b4
Infocatcher <infocatcher.dev.null@gmail.com>
2022-05-21 23:12:18 +0300

    toggleToolbarRows.js: basic script to toggle multiline toolbar

---
+++ toggleToolbarRows.js
@@ -0,0 +1,39 @@
+﻿var oSet = AkelPad.ScriptSettings();
+if(oSet.Begin("..\\ToolBar", 0x1 /*POB_READ*/)) {
+	var tbData = oSet.Read("ToolBarText", 3 /*PO_STRING*/);
+	oSet.End();
+}
+
+if(tbData && oSet.Begin("..\\ToolBar", 0x2 /*POB_SAVE*/)) {
+	var tbText = hexToStr(tbData);
+	tbText = tbText.replace(/(#?)BREAK/g, function(s, commented) {
+		return (commented ? "" : "#") + "BREAK";
+	});
+	tbData = strToHex(tbText);
+	oSet.Write("ToolBarText", 3 /*PO_STRING*/, tbData);
+	oSet.End();
+
+	if(AkelPad.IsPluginRunning("ToolBar::Main")) {
+		AkelPad.Call("ToolBar::Main");
+		AkelPad.Call("ToolBar::Main");
+	}
+}
+
+function hexToStr(h) {
+	return h.replace(/[\dA-F]{4}/g, function(h) {
+		var b1 = h.substr(0, 2);
+		var b2 = h.substr(2);
+		var n = parseInt(b2 + b1, 16);
+		return String.fromCharCode(n);
+	});
+}
+function strToHex(s) {
+	return s.replace(/[\s\S]/g, function(c) {
+		var n = c.charCodeAt(0);
+		var h = n.toString(16).toUpperCase();
+		h = "0000".substr(h.length) + h;
+		var b1 = h.substr(0, 2);
+		var b2 = h.substr(2);
+		return b2 + b1;
+	});
+}
