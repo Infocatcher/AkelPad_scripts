8dc7be90f9430f85f7dd7c55abb223de8027fef5
Infocatcher <infocatcher.dev.null@gmail.com>
2022-07-12 19:47:55 +0300

    Add description for -unformattedDelimiter

--- jsBeautifier.js
+++ jsBeautifier.js
@@ -47,6 +47,7 @@
 //   -detectPackers=true           - detect packers and obfuscators
 
 //   -unformattedTags=["a"]        - list of tags, that shouldn't be reformatted (only for HTML)
+//   -unformattedDelimiter=""      - keep text content together between this string
 //   -voidElements=["br"]          - list of HTML void elements - aka self-closing tags - aka singletons
 //   -indentInnerHTML=true         - indent content inside <html> (only for HTML)
 //   -indentHeadInnerHTML=true     - indent content inside <head> (only for HTML)
