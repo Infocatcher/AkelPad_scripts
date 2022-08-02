e7d55b6e1ca2573af30bba7847c6344a29ef9719
Infocatcher <infocatcher.dev.null@gmail.com>
2022-08-02 22:08:11 +0300

    Bump version to 0.3.1 - 2022-08-02

--- jsBeautifier.js
+++ jsBeautifier.js
@@ -4,8 +4,8 @@
 // https://github.com/Infocatcher/AkelPad_scripts/blob/master/jsBeautifier.js
 // https://github.com/Infocatcher/AkelPad_scripts/blob/master/Include/jsBeautifier_tests.js
 
-// (c) Infocatcher 2011-2019
-// Version: 0.3.0 - 2019-09-21
+// (c) Infocatcher 2011-2022
+// Version: 0.3.1 - 2022-08-02
 // Author: Infocatcher
 // Based on scripts from https://beautifier.io/ (http://jsbeautifier.org/)
 // [built from https://github.com/beautify-web/js-beautify/tree/release 2022-06-20 23:53:03 UTC]
