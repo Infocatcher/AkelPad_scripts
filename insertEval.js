12baca5854dc0136fae7ad41a3ed090ebcfd6bb7
Infocatcher <infocatcher.dev.null@gmail.com>
2023-03-11 23:35:30 +0300

    Experimental: don't unformat commented data

--- insertEval.js
+++ insertEval.js
@@ -135,9 +135,14 @@ var utils = {
 			|| /\d+,\d+,\d+/.test(s) // Expression like [1,2,3]
 		)
 			return s;
-		return s
-			.replace(/(\d+)[ \xa0](?=\d)/g, "$1") // 12 345 -> 12345
-			.replace(/(\d+),(\d+)(?!,)/g, "$1.$2"); // 1,23 -> 1.23
+		// Try ignore comments:
+		// Handled data // commented
+		return s.replace(/^([^\r\n]*?)(\/\/[^\r\n]*)?$/mg, function(s, start, comment) {
+			return start
+				.replace(/(\d+)[ \xa0](?=\d)/g, "$1") // 12 345 -> 12345
+				.replace(/(\d+),(\d+)(?!,)/g, "$1.$2") // 1,23 -> 1.23
+				+ (comment || "");
+		});
 	},
 
 	append: function(s) {
