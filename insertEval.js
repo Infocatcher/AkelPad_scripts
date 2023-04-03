01a96ad67c945a7737db2a881adbb299dc95850e
Infocatcher <infocatcher.dev.null@gmail.com>
2023-04-04 00:04:36 +0300

    Add =raw alias for =r

--- insertEval.js
+++ insertEval.js
@@ -145,7 +145,7 @@ var utils = {
 	p: function(n) {
 		return toLocaleNum(formatNum(n));
 	},
-	r: function(n) { // Leave as is, e.g. for raw result of formatted expression
+	raw: function(n) { // Leave as is, e.g. for raw result of formatted expression
 		return n;
 	},
 
@@ -209,6 +209,7 @@ utils.h = utils.x = utils.hex;
 utils.H = utils.X = utils.HEX;
 utils.o = utils.oct;
 utils.b = utils.bin;
+utils.r = utils.raw;
 utils.window = utils;
 
 function formatNum(n) {
@@ -256,7 +257,7 @@ function calc(expr, forceAsk) {
 		/^\s*=\s*/.test(expr)
 		// 2+2= (since 2018-08-21)
 		// 2+2=b -> 0b100 (since 2018-09-03)
-		|| /\s*=\s*(0?[xob]|oct|bin|h(ex)?|p|r)?$/i.test(expr)
+		|| /\s*=\s*(0?[xob]|oct|bin|h(ex)?|p|r(aw)?)?$/i.test(expr)
 	) {
 		expr = RegExp.rightContext || RegExp.leftContext;
 		resType = getConverter(RegExp.$1);
@@ -300,8 +301,8 @@ function calc(expr, forceAsk) {
 		return; // Cancel
 	if(newExpr != res) {
 		if(
-			/^\s*=\s*(0?[xob]|oct|bin|h(ex)?|p|r)?$/i.test(newExpr) // "=", "=p", "=x" & Co
-			|| /^\s*(0?[xob]|oct|bin|h(ex)?|p|r)$/i.test(newExpr) // "p", "x" & Co (without "=", looks like typo)
+			/^\s*=\s*(0?[xob]|oct|bin|h(ex)?|p|r(aw)?)?$/i.test(newExpr) // "=", "=p", "=x" & Co
+			|| /^\s*(0?[xob]|oct|bin|h(ex)?|p|r(aw)?)$/i.test(newExpr) // "p", "x" & Co (without "=", looks like typo)
 		) {
 			var rt = getConverter(RegExp.$1);
 			if(rt)
