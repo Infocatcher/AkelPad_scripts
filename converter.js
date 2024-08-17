Infocatcher/AkelPad_scripts

commit ea94aa5267f10256ba4158f2d155004a0a0cfd34

Refactor: add u() function to convert info \uhhhh format

converter.js
@@ -2510,8 +2510,7 @@ function encodeEntities(str, entities) {
 		if(entitiesBlackList && entity in entitiesBlackList)
 			continue;
 		var chr = entities[entity];
-		var hex = chr.charCodeAt(0).toString(16);
-		hex = "\\u" + ("000" + hex).slice(-4);
+		var hex = u(chr);
 		str = str.replace(new RegExp(hex, "g"), "&" + entity + ";");
 	}
 	return str;
@@ -2564,15 +2563,12 @@ function decodeHTML(str) {
 		.replace(/&quot;/g, '"')
 		.replace(/&amp;/g, "&");
 }
+function u(h) {
+	return "\\u" + ("000" + h).slice(-4);
+}
 
 function encodeEscapes(str) {
-	return str.replace(
-		/[^!-~ \t\n\r]/ig,
-		function(s) {
-			var hex = s.charCodeAt(0).toString(16);
-			return "\\u" + ("000" + hex).slice(-4);
-		}
-	);
+	return str.replace(/[^!-~ \t\n\r]/ig, u);
 }
 function decodeEscapes(str) {
 	if(customEscapesDecoder)

commit d46f32380769d180b90efabdf8bb7246835a29e4

Fix encodeEscapes() (regression in ea94aa5267f10256ba4158f2d155004a0a0cfd34 commit)

converter.js
@@ -2584,11 +2584,11 @@ function decodeHTML(str) {
 function uu(s) {
 	var out = "";
 	for(var i = 0, l = s.length; i < l; ++i)
-		out += u(s.charCodeAt(i).toString(16));
+		out += u(s.charAt(i));
 	return out;
 }
-function u(h) {
-	return "\\u" + ("000" + h).slice(-4);
+function u(c) {
+	return "\\u" + ("000" + c.charCodeAt(0).toString(16)).slice(-4);
 }
 
 function encodeEscapes(str) {